fa2a060c0cc6c9c71ee196a242066aa6e6e5b9e3
MFBenedetto <mbenedetto@gmail.com>
2022-04-01 22:11:10 +0200

    summary

--- summary.py
+++ summary.py
@@ -1,3 +1,4 @@
+from tkinter import X
 import dash
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
@@ -7,50 +8,109 @@ from datetime import date
 import plotly.express as px
 import plotly.figure_factory as ff
 import numpy as np
+import plotly.graph_objects as go
+
+
+m1 = go.Figure()
+m1.add_trace(go.Indicator(
+    mode = "number+delta",
+    value = 1150,
+    delta = {'reference': 800, 'relative': True},
+    domain = {'x': [0, 1], 'y': [0, 1]}))
+
+m1.update_layout(
+    title={
+        'text': "<span style='color:black'>ACCOUNTS</span><br><span style='color:gray'>ACTIVE</span>",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+            'yanchor': 'top'},
+    font= {'size':24}
+    )
+
+m2 = go.Figure(go.Indicator(
+    mode = "number+delta",
+    value = 492,
+    delta = {"reference": 500, "valueformat": ".0f"},
+    domain = {'y': [0, 1], 'x': [0.25, 0.75]}))
+
+m2.add_trace(go.Scatter(
+    y = [325, 324, 405, 400, 424, 404, 417, 432, 419, 394, 410, 426, 413, 419, 404, 408, 401, 377, 368, 361, 356, 359, 375, 397, 394, 418, 437, 450, 430, 442, 424, 443, 420, 418, 423, 423, 426, 440, 437, 436, 447, 460, 478, 472, 450, 456, 436, 418, 429, 412, 429, 442, 464, 447, 434, 457, 474, 480, 499, 497, 480, 502, 480, 492]))
+
+m2.update_layout(
+    xaxis = {'range': [0, 62]},
+    title={
+        'text': "<span style='color:black'>USERS ONLINE</span>",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    font= {'size':24}
+    )
+
+m3 = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    value = 450,
+    domain = {'x': [0, 1], 'y': [0, 1]}
+))
+
+
+m3.update_layout(
+    title={
+        'text': "<span style='color:black'>SUBSCRIPTIONS</span>",
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    font= {'size':24}
+    )
 
 df = px.data.tips()
-fig1 = px.histogram(df, x="total_bill", y="tip", color="sex", marginal="rug",
+df['customer'] = df['sex'].replace({'Female': 'Normal', 'Male': 'Premium'})
+df['Bill'] = df['total_bill']
+fig1 = px.histogram(df, x="Bill", y="Bill", color="customer", marginal="rug",
                    hover_data=df.columns)
 
-
 # Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-x4 = np.random.randn(200) + 4
+x1 = np.random.randn(200) + 8
+x2 = np.random.randn(200) + 10
+x3 = np.random.randn(200) + 9
+x4 = np.random.randn(200) + 14
 
 # Group data together
 hist_data = [x1, x2, x3, x4]
 
 group_labels = ['Group 1', 'Group 2', 'Group 3', 'Group 4']
 
+fig1.update_layout(height=450)
+
 # Create distplot with custom bin_size
 fig2 = ff.create_distplot(hist_data, group_labels, bin_size=.2)
 
-nav = create_navbar()
+fig2.update_layout(height=450)
 
-fig3 =dict(
+dates = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+fig3 = dict(
             data=[
                 dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                    2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
-                    350, 430, 474, 526, 488, 537, 500, 439],
+                    x=dates,
+                    y=[0.03, 0.02, 0.08, 0.16, 0.01, 0.14, 0.11, 0.19, 0.21, 0.1, 0.06, 0.23, 0.17, 0.24, 0.07, 0.15, 0.04, 0.05],
                     name='Rest of world',
-                    marker=dict(
-                        color='rgb(55, 83, 109)'
-                    )
                 ),
                 dict(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                    2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                    299, 340, 403, 549, 499],
+                    x=dates,
+                    y=[0.82, 0.81, 0.86, 0.9, 0.89, 0.88, 0.87, 0.99, 0.92, 0.94, 0.98, 0.85, 0.96, 0.93, 0.95, 0.8, 0.97, 0.83],
                     name='China',
-                    marker=dict(
-                        color='rgb(26, 118, 255)'
-                    )
-                )
+                ),
+                dict(
+                    x=dates,
+                    y=[0.68, 0.5, 0.34, 0.74, 0.19, 0.04, 0.77, 0.33, 0.84, 0.37, 0.97, 0.73, 0.11, 0.94, 0.61, 0.13, 0.1, 0.44],
+                    name='Europe',
+                ),
+                dict(
+                    x=dates,
+                    y=[0.8, 0.44, 0.45, 0.82, 0.56, 0.83, 0.9, 0.49, 0.92, 0.97, 0.58, 0.61, 0.76, 0.51, 0.71, 0.84, 0.41, 0.5],
+                    name='USA')
             ],
             layout=dict(
                 showlegend=True,
@@ -58,90 +118,52 @@ fig3 =dict(
                     x=0,
                     y=1.0
                 ),
-                margin=dict(l=40, r=0, t=40, b=30)
+                margin=dict(l=40, r=0, t=40, b=30),
             )
         )
 
+nav = create_navbar()
 
 def create_summary_page():
     layout = html.Div([
             nav,
-            dbc.Row([dbc.Col(html.H1("SUMMARY"),
-                        width={'size': 2, 'offset': 5}),
-                    dbc.Col(dcc.DatePickerRange(
+            dbc.Col(dcc.DatePickerRange(
                                 id='date-picker-range',
-                                start_date=date(1997, 5, 3),
-                                end_date= date(2022, 3, 31),
-                            ),
-                        width={'size': 2, 'offset': 2})
-                    ], justify="center",  align="end"),
-            dbc.Row([dbc.Col(html.H2("Metric 1"),
-                            width={'size':2, 'offset':2}
-                            ),
-                    dbc.Col(html.H2("Metric 2"),
-                            width={'size':2, 'offset':1}),
-                    dbc.Col(html.H2("Metric 3"),
-                            width={'size':2, 'offset':1}),
-            ]),
-            dbc.Row(
-                [
-                    dbc.Col(dcc.Dropdown(id='metric_1', placeholder='last dropdown',
-                                        options=[{'label': 'Option A', 'value': 'optA'},
-                                                {'label': 'Option B', 'value': 'optB'}]),
-                            width={'size': 3, "offset": 2, 'order': 3}
-                            ),
-                    dbc.Col(dcc.Dropdown(id='metric_2', placeholder='first dropdown',
-                                        options=[{'label': 'Option A', 'value': 'optA'},
-                                                {'label': 'Option B', 'value': 'optB'}]),
-                            width={'size': 4, "offset": 1, 'order': 1}
-                            ),
-                    dbc.Col(dcc.Dropdown(id='metric_3', placeholder='middle dropdown',
-                                        options=[{'label': 'Option A', 'value': 'optA'},
-                                                {'label': 'Option B', 'value': 'optB'}]),
-                            width={'size': 2,  "offset": 0, 'order': 2}
-                            ),
-                ]
-            ),
+                                start_date=date(2022, 1, 1),
+                                end_date= date(2022, 3, 31), style={'textAlign': 'center'}),
+                                width={'size': 4, 'offset':1}),
+            html.H1("SUMMARY", style={'textAlign': 'center'}),
+            dbc.Row([dbc.Col(dcc.Graph(id='m1', figure=m1),
+                        id='fig1',
+                        width={'size': 3,  "offset": 0, 'order': 1}),
+                    dbc.Col(dcc.Graph(id='m2', figure=m2),
+                            width={'size': 5,  "offset": 0, 'order': 2}),
+                    dbc.Col(dcc.Graph(id='m3', figure=m3),
+                            width={'size': 3,  "offset": 0, 'order': 3})],
+                justify="center", align='end'),
             html.Hr(),
-            dbc.Row([dbc.Col(html.H2("Sales timeline"),
-                        width={'size': 3, 'offset':1}),
+            dbc.Row([dbc.Col(html.H2("Client expenditure"),
+                        width={'size': 4, 'offset':0},
+                        style = {'text-align':'center'}),
                     dbc.Col(html.H2("Revenue timeline"),
-                        width={'size': 3, 'offset':1})],
-                    justify="center", align="end"),
+                        width={'size': 4, 'offset':0},
+                        style = {'text-align':'center'})],
+                    justify="evenly", align="end"),
             dbc.Row([dbc.Col(dcc.Graph(id='fig1', figure=fig1),
                         id='fig1',
-                        width={'size': 4,  "offset": 2, 'order': 'first'}),
-                    dbc.Col(dcc.Graph(id='fig2', figure=fig1),
-                            width={'size': 4,  "offset": 1, 'order': 'last'})
-            ]),
+                        width={'size': 4,  "offset": 0, 'order': 'first'}),
+                    dbc.Col(dcc.Graph(id='fig2', figure=fig2),
+                            width={'size': 4,  "offset": 0, 'order': 'last'})
+            ], justify="evenly"),
             html.Hr(),
-            dbc.Row([dbc.Col(html.H2("Historic timeline"),
+            dbc.Row([dbc.Col(html.H2("Market share timeline"),
                             width={'size': 'auto', 'offset': 0}
                             )],justify="center", align="end"
                     ),
             dbc.Row([dbc.Col(dcc.Graph(id='fig3', figure=fig3),
                         style={'height': 120},
                         id='fig1',
-                        width={'size': 8,  "offset": 2, 'order': 'first'})
-            ])
+                        width={'size': 10,  "offset": 0, 'order': 'first'})
+            ],justify="evenly", align="end")
     ])
     return layout
-
-@callback(
-    [Output('pie_chart1', 'figure'),
-    Output('pie_chart2', 'figure')],
-    [Input('a_dropdown', 'value'),
-    Input('b_dropdown', 'value'),
-    Input('c_dropdown', 'value')]
-)
-def update_graph(dpdn_a, dpdn_b, dpdn_c):
-    dff = df[:200]
-    if dpdn_a is None or dpdn_b is None or dpdn_c is None:
-        pie_fig = px.pie(dff, names=dff.index, values='Street_robbery', title='Street Robbery Berlin')\
-            .update_layout(showlegend=False, title_x=0.5).update_traces(textposition='inside',  textinfo='label+percent')
-        pie_fig2 = px.pie(dff, names=dff.index, values='Drugs', title='Drugs Berlin')\
-            .update_layout(showlegend=False, title_x=0.5).update_traces(textposition='inside', textinfo='label+percent')
-        return pie_fig, pie_fig2
-    else:
-        raise dash.exceptions.PreventUpdate
-
