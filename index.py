ba382ceadac983c84422a037815bd812f7f6df87
MFBenedetto <mbenedetto@gmail.com>
2022-04-20 00:26:15 +0200

    update

--- index.py
+++ index.py
@@ -1,18 +1,14 @@
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-
 from dash import dcc, html, Dash
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 
 from home import create_home_page
-from data import create_data_page
-from summary import create_summary_page
-from ML import create_ML_page
-from business import create_business_page
-from reporting import create_reporting_page
-from config.USERS import USERNAME_PASSWORD_PAIRS
+# from data import create_data_page
+# from summary import create_summary_page
+# from ML import create_ML_page
+# from business import create_business_page
+# from reporting import create_reporting_page
+# from config.USERS import USERNAME_PASSWORD_PAIRS
 
 
 app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUX], update_title='Loading...')
@@ -36,19 +32,19 @@ app.title = 'Data and analytics dashboard - HOME'
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/data':
-        return create_data_page()
-    if pathname == '/summary':
-        return create_summary_page()
-    if pathname == '/ML':
-        return create_ML_page()
-    if pathname == '/business':
-        return create_business_page()
-    if pathname == '/reporting':
-        return create_reporting_page()
-    else:
-        return create_home_page()
-
+    # if pathname == '/data':
+    #     return create_data_page()
+    # if pathname == '/summary':
+    #     return create_summary_page()
+    # if pathname == '/ML':
+    #     return create_ML_page()
+    # if pathname == '/business':
+    #     return create_business_page()
+    # if pathname == '/reporting':
+    #     return create_reporting_page()
+    # else:
+    #     return create_home_page()
+    return create_home_page()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
